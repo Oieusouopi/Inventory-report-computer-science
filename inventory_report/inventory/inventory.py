269a6b025283c9ef5ee5e150140683caab8ef5d1
Oieusouopi <rafasc866@gmail.com>
2022-09-21 14:37:56 -0300

    inventory json and xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+from bs4 import BeautifulSoup
 from reports.simple_report import SimpleReport
 from reports.complete_report import CompleteReport
 
@@ -16,5 +18,32 @@ class Inventory:
                     return CompleteReport.generate(report_list)
                 else:
                     raise "Algo deu errado"
+        elif path[-4] == "json":
+            with open(path) as file:
+                report_list = json.load(file)
+                if type == "simples":
+                    return SimpleReport.generate(report_list)
+                elif type == "completo":
+                    return CompleteReport.generate(report_list)
+                else:
+                    raise "Algo deu errado"
+        elif path[-4] == ".xml":
+            with open(path, "r") as file:
+                report = file.read()
+                report_list = BeautifulSoup(report, "xml")
+                if type == "simples":
+                    return SimpleReport.generate(report_list)
+                elif type == "completo":
+                    return CompleteReport.generate(report_list)
+                else:
+                    raise "Algo deu errado"
         else:
             raise "Error arquivo invalido"
+
+    # def type_data(cls, type, report_list):
+    #     if type == "simples":
+    #         return SimpleReport.generate(report_list)
+    #     elif type == "completo":
+    #         return CompleteReport.generate(report_list)
+    #     else:
+    #         raise "Algo deu errado"
