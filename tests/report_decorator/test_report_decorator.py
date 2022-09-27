055908f4866db4693bee85cb2b19a238401c371c
Oieusouopi <rafasc866@gmail.com>
2022-09-27 19:38:26 -0300

    test_report_decorator.py

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,22 +1,21 @@
 from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    colored_report = ColoredReport("simples")
-
-    assert colored_report.generate(
-            1,
-            "Cafe",
-            "Cafes Nature",
-            "2020-07-04",
-            "2023-02-09",
-            "FR48",
-            "instrucao",
-    ) == (
-        "\033[32mData de fabricação mais antiga:\033[0m "
-        + "\033[36m04-07-2020\033[0m\n"
-        + "\033[32mData de validade mais próxima:\033[0m "
-        + "\033[36m2023-02-09\033[0m\n"
-        + "\033[32mEmpresa com mais produtos: \033[0m "
-        + "\033[31mCafes Nature\033[0m"
-    )
+    colored_report = ColoredReport(SimpleReport)
+    report = colored_report.generate([{
+        "id": 1,
+        "nome_do_produto": "Cafe",
+        "nome_da_empresa": "Cafes Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "instrucao"
+    }])
+    assert "\033[32mData de fabricação mais antiga:\033[0m" in report
+    assert "\033[36m2020-07-04\033[0m\n" in report
+    assert "\033[32mData de validade mais próxima:\033[0m" in report
+    assert "\033[36m2023-02-09\033[0m\n" in report
+    assert "\033[32mEmpresa com mais produtos:\033[0m" in report
+    assert "\033[31mCafes Nature\033[0m" in report
